7fdaf4bdb890ea62f6b664549282c15b618ac2cb
sebbur0910 <sebbur0910@highgateschool.org.uk>
2023-02-09 10:31:40 +0000

    activities

--- student_activities/console_commands.py
+++ student_activities/console_commands.py
@@ -1,16 +1,40 @@
 from sqlalchemy import create_engine
+
 from sqlalchemy.orm import Session
-from models import Person
+
+from models import Person, Activity
 
 # Create some instances of the Person class
-andrew = Person(first_name="Andrew", last_name="Dales")
-people = [Person(first_name="Chris", last_name="Brolin"), Person(first_name='Vera', last_name="Malcova")]
+
+people = [Person(first_name="Andrew", last_name="Dales"),
+
+          Person(first_name="Chris", last_name="Brolin"),
+
+          Person(first_name='Vera', last_name="Malcova"),
+
+          ]
+
+chess = Activity(name="Chess")
+
+fives = Activity(name="Fives")
+
+outdoor_ed = Activity(name="Outdoor Ed")
+
+people[0].activities.append(chess)
+
+people[0].activities.append(fives)
+
+people[1].activities.append(outdoor_ed)
+
+people[1].activities.append(fives)
 
 # Connect to the activities database
+
 engine = create_engine('sqlite:///activities.db', echo=True)
 
 # Create a session and add the people to the database
+
 with Session(engine) as sess:
-    sess.add(andrew)
     sess.add_all(people)
+
     sess.commit()
--- student_activities/models.py
+++ student_activities/models.py
@@ -1,8 +1,34 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Table, UniqueConstraint, ForeignKey
+from sqlalchemy.orm import declarative_base, backref
+from sqlalchemy.orm import relationship
 
 # Base is called an Abstract Base Class - our SQL Alchemy models will inherit from this class
 Base = declarative_base()
+# Sets up a link table with activity_id and person_id as foreign keys
+# Base.metadata is a container object that keeps together many different features of the database
+person_activity = Table('person_activity',
+                        Base.metadata,
+                        Column('id', Integer, primary_key=True),
+                        Column('activity_id', ForeignKey('activity.id')),
+                        Column('person_id', ForeignKey('person.id')),
+                        UniqueConstraint('activity_id', 'person_id')
+                        )
+
+
+# Sets up an Activity table, this references "attendees" via the person_activities table
+class Activity(Base):
+    __tablename__ = 'activity'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, unique=True, nullable=False)
+    loaction_id = Column(Integer, ForeignKey("location.id"), default=None)
+    attendees = relationship("Person",
+                             secondary=person_activity,
+                             order_by='(Person.last_name, Person.first_name)',
+                             back_populates="activities")
+
+    # Gives a representation of an Activity (for printing out)
+    def __repr__(self):
+        return f"<Activity({self.name})>"
 
 
 # Sets up a Person table, this references "activities" via the person_activities table
@@ -11,6 +37,10 @@ class Person(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     first_name = Column(String, nullable=False)
     last_name = Column(String, nullable=False)
+    activities = relationship("Activity",
+                              secondary=person_activity,
+                              order_by='Activity.name',
+                              back_populates="attendees")
 
     # Gives a representation of a Person (for printing out)
     def __repr__(self):
@@ -19,3 +49,13 @@ class Person(Base):
     # Include a method:
     def greeting(self):
         print(f'{self.first_name} says "hello"!')
+
+class Location(Base):
+    __tablename__ = 'location'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    room = Column(String, nullable=False)
+    activities = relationship("Activity",
+                              backref=backref("location"))
+    # Gives a representation of a Person (for printing out)
+    def __repr__(self):
+        return f"<Person({self.first_name} {self.last_name})>"
